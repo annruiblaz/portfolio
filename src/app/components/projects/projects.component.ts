a2b0b37a9fe77318622c419175d3f715b93238ba
annruiblaz <anna.ruiz.dev@gmail.com>
2025-02-26 23:30:50 +0100

    link deploy country-spa fixed

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -22,7 +22,7 @@ export class ProjectsComponent {
       title: 'Country SPA',
       subtitle: 'Web creada como parte del curso de Udemy de Fernando Herrera, para buscar países y visualizar su información. Con estilos y funcionalidades propias añadidas.',
       img: ['assets/images/Mockup-CountrySPA-1.png','assets/images/Mockup-CountrySPA-2.png'],
-      urlDemo: 'https://annruiblaz.github.io/03-country-spa/countries/by-country',
+      urlDemo: 'https://anna-country-spa.netlify.app/countries/by-country',
       urlGithub: 'https://github.com/annruiblaz/03-country-spa',
       stackUsed: [
         { name: 'Angular', color: 'red'}
